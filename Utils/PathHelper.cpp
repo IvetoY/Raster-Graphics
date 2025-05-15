b5c0d79cceeb3ad7f9011c2f84f5e7dcba743e1e
IvetoY <ivelinabeti@gmail.com>
2025-05-15 20:33:03 +0300

    Папка Utils с файлове за взимане на разширението на файла

---
+++ Utils/PathHelper.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <cstring>
+#include "PathHelper.h"
+
+const char* extractFileExtension(const char* filePath){
+    size_t filePathLength = strlen(filePath);
+    if(!filePath) {return nullptr;}
+    if(filePathLength==0) {return nullptr;}
+    const char* end = filePath + filePathLength - 1;
+    const char* iterator = end;
+    while(filePathLength>0 && *iterator!='.'){
+        iterator --;
+        filePathLength --;
+    }
+    if(filePathLength == 0){return "";}
+    return iterator;
+}
