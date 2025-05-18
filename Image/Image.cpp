ca016d737789776058b981a941613ca3f989e523
IvetoY <ivelinabeti@gmail.com>
2025-05-18 13:18:40 +0300

    Добавен масив от пиксели

--- Image/Image.cpp
+++ Image/Image.cpp
@@ -4,12 +4,34 @@ Image::Image() : width(0), height(0), maxColourNumbers(0), magicNumber(new char[
     magicNumber[0] = '\0';
     fileName[0] = '\0';
 }
-Image::Image(unsigned _w, unsigned _h, uint8_t _m,const String& _mN, const String& _fN) : width(_w), height(_h), maxColourNumbers(_m), magicNumber(_mN), fileName(_fN) {}
+Image::Image(unsigned _w, unsigned _h, uint8_t _m,const String& _mN, const String& _fN, std::vector<std::vector<Pixel>> _p) : width(_w), height(_h), maxColourNumbers(_m), magicNumber(_mN), fileName(_fN), pixels(_p) {}
+Image::Image(const Image& other) : width(other.width), height(other.height),maxColourNumbers(other.maxColourNumbers), magicNumber(other.magicNumber), fileName(other.fileName), pixels(other.pixels) {}
 unsigned Image::getHeight() const {return height;}
 unsigned Image::getWidth() const {return width;}
 const String& Image::getMagicNumber() const{return magicNumber;}
 const String& Image::getFileName() const {return fileName;}
+void Image::setFileName(const String& name){
+    if (name != "" && name != " ")
+	{
+		fileName= name;
+		return;
+	}
 
+	throw std::invalid_argument("Name must not be empty or contain only spaces");
+}
+
+Pixel& Image::at(unsigned x, unsigned y) {
+    if (x >= width || y >= height) {
+        throw std::out_of_range("Pixel coordinates out of range");
+    }
+    return pixels[y][x];
+}
+const Pixel& Image::at(unsigned x, unsigned y) const {
+    if (x >= width || y >= height) {
+        throw std::out_of_range("Pixel coordinates out of range");
+    }
+    return pixels[y][x];
+}
 void Image::print(std::ostream& os) const{
     os<<fileName<<std::endl;
 }
--- Image/Image.h
+++ Image/Image.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdint>
+#include <vector>
+#include "../Pixel/Pixel.h"
 #include "../String/String.h"
 class Image{
     protected:
@@ -8,15 +10,22 @@ class Image{
     uint8_t maxColourNumbers; // Maximum color value 0-255 for 8-bit RGB
     String magicNumber;
     String fileName;
+    std::vector<std::vector<Pixel>> pixels;
     //Vector<?> awaitingTransformations;
     public:
     Image();
-    Image(unsigned _w, unsigned _h, uint8_t _m, const  String& _mN,const String& _fN); //add vector for transformations
+    Image(unsigned _w, unsigned _h, uint8_t _m, const  String& _mN,const String& _fN, std::vector<std::vector<Pixel>> _p); //add vector for transformations
+    Image(const Image& other);
     virtual ~Image() = default;
     unsigned getWidth() const;
     unsigned getHeight() const;
     const String& getMagicNumber() const;
     const String& getFileName() const;
+
+    void setFileName(const String& name);
+
+    Pixel& at(unsigned x, unsigned y);
+    const Pixel& at(unsigned x, unsigned y) const;
 
     void print(std::ostream& os) const;
     
